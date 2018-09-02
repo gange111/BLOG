9f8cb150219043a60baf47082c19a55934d8c8d7
lixingchao <lixingchao@easted.com.cn>
2018-09-03 00:21:46 +0800

    add Tag

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from taggit.managers import TaggableManager
 
 
 class Post(models.Model):
@@ -21,6 +22,7 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft')
+    tags = TaggableManager()
 
     class Meta:
         ordering = ('-publish', )
